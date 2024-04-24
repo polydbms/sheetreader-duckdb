dd3c8b645f817e4f23387dfa46343eca7a09cea2
Jonas Benn <mail@benn.in>
2024-04-24 19:49:59 +0200

    Automatic renaming

---
+++ src/sheetreader_extension.cpp
@@ -0,0 +1,70 @@
+#define DUCKDB_EXTENSION_MAIN
+
+#include "sheetreader_extension.hpp"
+#include "duckdb.hpp"
+#include "duckdb/common/exception.hpp"
+#include "duckdb/common/string_util.hpp"
+#include "duckdb/function/scalar_function.hpp"
+#include "duckdb/main/extension_util.hpp"
+#include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
+
+// OpenSSL linked through vcpkg
+#include <openssl/opensslv.h>
+
+namespace duckdb {
+
+inline void SheetreaderScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
+    auto &name_vector = args.data[0];
+    UnaryExecutor::Execute<string_t, string_t>(
+	    name_vector, result, args.size(),
+	    [&](string_t name) {
+			return StringVector::AddString(result, "Sheetreader "+name.GetString()+" 🐥");;
+        });
+}
+
+inline void SheetreaderOpenSSLVersionScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
+    auto &name_vector = args.data[0];
+    UnaryExecutor::Execute<string_t, string_t>(
+	    name_vector, result, args.size(),
+	    [&](string_t name) {
+			return StringVector::AddString(result, "Sheetreader " + name.GetString() +
+                                                     ", my linked OpenSSL version is " +
+                                                     OPENSSL_VERSION_TEXT );;
+        });
+}
+
+static void LoadInternal(DatabaseInstance &instance) {
+    // Register a scalar function
+    auto sheetreader_scalar_function = ScalarFunction("sheetreader", {LogicalType::VARCHAR}, LogicalType::VARCHAR, SheetreaderScalarFun);
+    ExtensionUtil::RegisterFunction(instance, sheetreader_scalar_function);
+
+    // Register another scalar function
+    auto sheetreader_openssl_version_scalar_function = ScalarFunction("sheetreader_openssl_version", {LogicalType::VARCHAR},
+                                                LogicalType::VARCHAR, SheetreaderOpenSSLVersionScalarFun);
+    ExtensionUtil::RegisterFunction(instance, sheetreader_openssl_version_scalar_function);
+}
+
+void SheetreaderExtension::Load(DuckDB &db) {
+	LoadInternal(*db.instance);
+}
+std::string SheetreaderExtension::Name() {
+	return "sheetreader";
+}
+
+} // namespace duckdb
+
+extern "C" {
+
+DUCKDB_EXTENSION_API void sheetreader_init(duckdb::DatabaseInstance &db) {
+    duckdb::DuckDB db_wrapper(db);
+    db_wrapper.LoadExtension<duckdb::SheetreaderExtension>();
+}
+
+DUCKDB_EXTENSION_API const char *sheetreader_version() {
+	return duckdb::DuckDB::LibraryVersion();
+}
+}
+
+#ifndef DUCKDB_EXTENSION_MAIN
+#error DUCKDB_EXTENSION_MAIN not defined
+#endif
