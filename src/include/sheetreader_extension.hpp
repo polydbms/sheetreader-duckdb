8f6e2dc1a7032d578adf78df724ee3304a57467a
Jonas Benn <mail@benn.in>
2024-05-02 19:00:33 +0000

    Add first hello world table function

--- src/include/sheetreader_extension.hpp
+++ src/include/sheetreader_extension.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "duckdb.hpp"
+#include "duckdb/function/function.hpp"
 
 namespace duckdb {
 
@@ -10,4 +11,51 @@ public:
 	std::string Name() override;
 };
 
+struct SheetreaderScanData : public TableFunctionData {
+public:
+	SheetreaderScanData();
+
+	// void Bind(ClientContext &context, TableFunctionBindInput &input);
+
+	// void InitializeReaders(ClientContext &context);
+	// void InitializeFormats();
+	// void InitializeFormats(bool auto_detect);
+	// void SetCompression(const string &compression);
+
+	// static unique_ptr<SheetreaderScanData> Deserialize(Deserializer &deserializer);
+
+public:
+	//! The files we're reading
+	vector<string> files;
+	
+	// Filereader
+
+	//! Whether or not we should ignore malformed JSON (default to NULL)
+	bool ignore_errors = false;
+	//! Maximum JSON object size (defaults to 16MB minimum)
+	idx_t maximum_object_size = 16777216;
+	//! Whether we auto-detect a schema
+	bool auto_detect = false;
+	//! Sample size for detecting schema
+	idx_t sample_size = idx_t(STANDARD_VECTOR_SIZE) * 10;
+	//! Max depth we go to detect nested JSON schema (defaults to unlimited)
+	idx_t max_depth = NumericLimits<idx_t>::Maximum();
+	//! We divide the number of appearances of each JSON field by the auto-detection sample size
+	//! If the average over the fields of an object is less than this threshold,
+	//! we default to the JSON type for this object rather than the shredded type
+	double field_appearance_threshold = 0.1;
+	//! The maximum number of files we sample to sample sample_size rows
+	idx_t maximum_sample_files = 32;
+	//! Whether we auto-detect and convert JSON strings to integers
+	bool convert_strings_to_integers = false;
+
+	//! All column names (in order)
+	vector<string> names;
+
+	//! The inferred avg tuple size
+	idx_t avg_tuple_size = 420;
+private:
+	SheetreaderScanData(ClientContext &context, vector<string> files);
+
+};
 } // namespace duckdb
--- src/sheetreader_extension.cpp
+++ src/sheetreader_extension.cpp
@@ -1,47 +1,78 @@
+#include "duckdb/common/multi_file_reader.hpp"
+#include "duckdb/common/types/value.hpp"
+#include "duckdb/common/types/vector.hpp"
+#include "duckdb/function/function.hpp"
 #define DUCKDB_EXTENSION_MAIN
 
-#include "sheetreader_extension.hpp"
 #include "duckdb.hpp"
 #include "duckdb/common/exception.hpp"
 #include "duckdb/common/string_util.hpp"
 #include "duckdb/function/scalar_function.hpp"
+#include "duckdb/function/table_function.hpp"
 #include "duckdb/main/extension_util.hpp"
-#include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
+#include "sheetreader_extension.hpp"
 
-// OpenSSL linked through vcpkg
-#include <openssl/opensslv.h>
+#include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
 
 namespace duckdb {
 
-inline void SheetreaderScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
-    auto &name_vector = args.data[0];
-    UnaryExecutor::Execute<string_t, string_t>(
-	    name_vector, result, args.size(),
-	    [&](string_t name) {
-			return StringVector::AddString(result, "Sheetreader "+name.GetString()+" 🐥");;
-        });
+SheetreaderScanData::SheetreaderScanData() {
 }
 
-inline void SheetreaderOpenSSLVersionScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
-    auto &name_vector = args.data[0];
-    UnaryExecutor::Execute<string_t, string_t>(
-	    name_vector, result, args.size(),
-	    [&](string_t name) {
-			return StringVector::AddString(result, "Sheetreader " + name.GetString() +
-                                                     ", my linked OpenSSL version is " +
-                                                     OPENSSL_VERSION_TEXT );;
-        });
+SheetreaderScanData::SheetreaderScanData(ClientContext &context, vector<string> files_p)
+    : files(std::move(files_p)) {
+	// InitializeReaders(context);
+	// InitializeFormats();
+}
+
+
+inline void SheetreaderTableFun(ClientContext &context, TableFunctionInput &data_p, DataChunk &output) {
+	// Create a single chunk with a single string column
+	Vector &column = output.data[0];
+	Value val = Value("Hello World");
+	column.SetValue(0, val);
+	output.SetCardinality(1);
+}
+
+inline unique_ptr<FunctionData> SheetreaderBindFun(ClientContext &context, TableFunctionBindInput &input,
+                                                   vector<LogicalType> &return_types, vector<string> &names) {
+
+	auto bind_data = make_uniq<SheetreaderScanData>();
+	// TODO: Do we need any information from info?
+	// TableFunctionInfo *info = input.info.get();
+
+	// Get the file names from the first parameter
+	vector<string> file_names = MultiFileReader::GetFileList(context, input.inputs[0], ".XLSX (Excel)");
+
+	// Here we could handle any named parameters
+	// for (auto &kv : input.named_parameters) {
+	// }
+
+	// Create a TableFunctionData object and return it
+	TableFunctionData tableFunctionData;
+	// tableFunctionData.Initialize(file_names);
+
+	return_types = {LogicalType::VARCHAR};
+	names = {"Hello World column"};
+
+	return make_uniq<TableFunctionData>(tableFunctionData);
 }
 
 static void LoadInternal(DatabaseInstance &instance) {
-    // Register a scalar function
-    auto sheetreader_scalar_function = ScalarFunction("sheetreader", {LogicalType::VARCHAR}, LogicalType::VARCHAR, SheetreaderScalarFun);
-    ExtensionUtil::RegisterFunction(instance, sheetreader_scalar_function);
+	// Register a table function
+	TableFunction sheetreader_table_function =
+	    TableFunction("sheetreader", {LogicalType::VARCHAR}, SheetreaderTableFun, SheetreaderBindFun);
+	ExtensionUtil::RegisterFunction(instance, sheetreader_table_function);
 
-    // Register another scalar function
-    auto sheetreader_openssl_version_scalar_function = ScalarFunction("sheetreader_openssl_version", {LogicalType::VARCHAR},
-                                                LogicalType::VARCHAR, SheetreaderOpenSSLVersionScalarFun);
-    ExtensionUtil::RegisterFunction(instance, sheetreader_openssl_version_scalar_function);
+	// // Register a scalar function
+	// auto sheetreader_scalar_function = ScalarFunction("sheetreader", {LogicalType::VARCHAR}, LogicalType::VARCHAR,
+	// SheetreaderScalarFun); ExtensionUtil::RegisterFunction(instance, sheetreader_scalar_function);
+
+	// // Register another scalar function
+	// auto sheetreader_openssl_version_scalar_function = ScalarFunction("sheetreader_openssl_version",
+	// {LogicalType::VARCHAR},
+	//                                             LogicalType::VARCHAR, SheetreaderOpenSSLVersionScalarFun);
+	// ExtensionUtil::RegisterFunction(instance, sheetreader_openssl_version_scalar_function);
 }
 
 void SheetreaderExtension::Load(DuckDB &db) {
@@ -56,8 +87,8 @@ std::string SheetreaderExtension::Name() {
 extern "C" {
 
 DUCKDB_EXTENSION_API void sheetreader_init(duckdb::DatabaseInstance &db) {
-    duckdb::DuckDB db_wrapper(db);
-    db_wrapper.LoadExtension<duckdb::SheetreaderExtension>();
+	duckdb::DuckDB db_wrapper(db);
+	db_wrapper.LoadExtension<duckdb::SheetreaderExtension>();
 }
 
 DUCKDB_EXTENSION_API const char *sheetreader_version() {
