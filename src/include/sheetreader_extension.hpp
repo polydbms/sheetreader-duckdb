fbb19fce2d02f26db8b32ae43000f03e87b14516
midhun <m.venugopalan@campus.tu-berlin.de>
2025-11-21 14:31:17 +0100

    Add missing Version() method to Extension class
    - Added Version() method declaration in header
    - Added Version() method implementation
    - Fixes 'marked override but does not override' error
    - Completes DuckDB v1.4.0 Extension API migration

--- src/include/sheetreader_extension.hpp
+++ src/include/sheetreader_extension.hpp
@@ -16,6 +16,7 @@ class SheetreaderExtension : public Extension {
 public:
 	void Load(DuckDB &db) override;
 	std::string Name() override;
+	std::string Version() const override;
 };
 
 //! Contains all data that is determined during the bind function
